7780ef41cb545bb81c1ba08145bda4265bfe3bb1
diegoingaramo <diegoingaramo@gmail.com>
2016-01-07 13:52:52 -0300

    routes spec. completed

--- index.js
+++ index.js
@@ -3,17 +3,25 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5001));
 
-app.get("/new/*", function(req, res) 
+/* 
+Search latest images search
+*/
+app.get("/api/latest/imagesearch", function(req, res) 
 {
-    res.end("hola");
+    res.end("api/latest/imagesearch/");
 });
 
-app.get("/*", function(req, res) {
-    res.end("hola");
+app.get("/api/imagesearch/:searchQuery/:offset", function(req, res) {
+
+    var searchQuery = req.params.searchQuery;
+    var offset = req.params.offset;
+
+    res.end("api/imagesearch/:searchQuery/:offset: " + searchQuery + " " + offset);
+
 });
 
-app.get(/.*/, function(req, res) {
-    res.end("hola");
+app.get("/", function(req, res) {
+    res.redirect(307,'/api/latest/imagesearch');
 });
 
 app.listen(app.get('port'), function() {
